a5a09dbffdbbfa427b1df426a9a3db6aa49c88bd
Paulo de Aguiar <paulodeaguiar@gmail.com>
2026-03-13 16:18:27 -0300

    Aula02-10032026

---
+++ Aula02-10032026/01_hello_world/src/com/hello/app/App.java
@@ -0,0 +1,6 @@
+package com.hello.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Mundo!");
+    }
+}
---
+++ Aula02-10032026/02_variaveis/src/com/variaveis/app/App.java
@@ -0,0 +1,15 @@
+package com.variaveis.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome;
+        nome = "Paulo Ribeiro de Aguiar";
+        int idade;
+        idade = 44;
+        double altura;
+        altura = 1.74;
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Altura: "+altura);
+    }
+}
